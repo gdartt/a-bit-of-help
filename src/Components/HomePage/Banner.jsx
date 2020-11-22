29a0dd1128db1ff7732bbce9e43578d2ca48bb2c
Alex Basystyi <alexbasisty@gmail.com>
2020-11-22 16:05:43 +0100

    header titles

--- src/Components/HomePage/Banner.jsx
+++ src/Components/HomePage/Banner.jsx
@@ -1,7 +1,12 @@
 
 const Banner = () => (
     <header className="header">
-        <div></div>
+        <div className="header-banner">
+            <div className="header-title">
+                <h2 className="light">Ludzie i nowe technologie</h2>
+                <h2 className="dark">Wsparcie dla organizacji pozarządowych</h2>
+            </div>
+        </div>
     </header>
 );
 
