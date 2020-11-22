be2091c2819be86788400c99787d8956d9135628
Alex Basystyi <alexbasisty@gmail.com>
2020-11-22 11:50:51 +0100

    scss variables and fonts with test scripts

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <h1>Hello, foundation!</h1>;
+  return (
+    <>
+      <h1>Hello, foundation!</h1>
+      <h2>base font</h2>
+      <h3>secondary font</h3>
+      <h4>alternative</h4>
+    </>
+  );
 }
 
 export default App;
