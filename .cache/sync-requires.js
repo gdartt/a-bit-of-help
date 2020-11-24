b6a394945c23b25d2c006362a52fffa0d91e5043
Rafał Michniuk <38009080+RafalMichniuk@users.noreply.github.com>
2020-11-24 21:10:32 +0100

    test

---
+++ .cache/sync-requires.js
@@ -0,0 +1,14 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\user\\Desktop\\Coding\\A BIT OF HELP\\strona-fundacji\\a-bit-of-help\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-jsx": hot(preferDefault(require("C:\\Users\\user\\Desktop\\Coding\\A BIT OF HELP\\strona-fundacji\\a-bit-of-help\\src\\pages\\404.jsx"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\user\\Desktop\\Coding\\A BIT OF HELP\\strona-fundacji\\a-bit-of-help\\src\\pages\\about.js"))),
+  "component---src-pages-hero-js": hot(preferDefault(require("C:\\Users\\user\\Desktop\\Coding\\A BIT OF HELP\\strona-fundacji\\a-bit-of-help\\src\\pages\\hero.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\user\\Desktop\\Coding\\A BIT OF HELP\\strona-fundacji\\a-bit-of-help\\src\\pages\\index.js")))
+}
+
