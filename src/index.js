0f7d5912f24ecbef8e56b789f061ca13cfc1f777
Rafał Michniuk <38009080+RafalMichniuk@users.noreply.github.com>
2020-11-28 22:11:55 +0100

    correct files structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/main.scss';
-import App from './Components/App/App';
+import './styles/main.scss';
+import App from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
