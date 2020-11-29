b74a26111102c7b708d4240fda7052efb7bd0f7e
Alex Basystyi <alexbasisty@gmail.com>
2020-11-29 18:22:47 +0100

    file structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 
 import Footer from '../Footer';
+import Banner from '../Banner';
 
 function App() {
   return (
     <div className="app">
         <p>A Bit Of Help</p>
         <Footer />
+        <Banner />
     </div>
   );
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './banner.scss';
+
+const Banner = () => {
+    return (
+        <div className="container">
+            Banner
+        </div>
+    )
+};
+
+export default Banner;
