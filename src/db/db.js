cd1e5b3eef6481dde665348fe1aa337c63ef3543
Alex Basystyi <alexbasisty@gmail.com>
2020-11-30 12:41:47 +0100

    database to render lists

---
+++ src/db/db.js
@@ -0,0 +1,23 @@
+
+export const aboutUsData = {
+    innovativeSolutions: [
+            'Pomagamy fundacjom, które pomagają innym, ale ze względu na brak środków finansowych nie mogą rozwijać się technologicznie',
+            'Realizujemy projekty, które usprawniają życie obywateli', 
+            'Wspieramy stowarzyszenia i organizacje pozarządowe działające na rzecz ludzi, zwierząt i środowiska oraz grupy w jakiś sposób wykluczonych cyfrowo', 
+            'Fundacja A BIT OF HELP opiera się wyłącznie na pracy wolontariuszy', 
+            'Nikt z nas nie pobiera żadnego wynagrodzenia za swoją pracę, a jedyną zapłatą za poświęcony czas i wysiłek jest satysfakcja naszych klientów oraz nasze doświadczenie zawodowe', 
+            'Naszym założeniem jest nieodpłatne tworzenie projektów korzystnych społecznie, jednocześnie pozwalając członkom fundacji na doskonalenie swoich umiejętności w wybranych przez siebie dziedzinach',
+        ],
+    whomWeHelp: [
+        'Dla nas każdy pomysł to nowe wyzwanie',
+        'Chcemy zdobywać doświadczenie i realizować projekty dla Was', 
+        'Nowoczesne rozwiązania technologiczne to nasza specjalność', 
+        'Łączy nas pasja tworzenia rzeczy społecznie pożytecznych', 
+        'Wierzymy, że dla wielu ludzi o dobrym sercu zaistnienie w internecie jest drogą do realizacji pomysłów związanych z niesieniem pomocy i usprawnianiem życia obywateli', 
+        'Jesteśmy grupą altruistów i pasjonatów technologii', 
+        'Specjalizujemy się w różnych dziedzinach i uzupełniamy się w działaniach',  
+        'Dzielimy się wiedzą, którą udało nam się dotychczas zdobyć', 
+        'Wiemy, że wszystko przenosi się do internetu i prawie wszyscy pracują teraz zdalnie', 
+        'Znamy się na cyfrowych rozwiązaniach, więc z przyjemnością wspomożemy Wasze przedsięwzięcia',
+    ],
+}
