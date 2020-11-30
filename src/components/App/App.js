b9be934195b782fb35a2e06280649936a7fa3dfd
Alex Basystyi <alexbasisty@gmail.com>
2020-11-30 12:24:15 +0100

    file structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import "./App.scss";
 
 import Footer from '../Footer';
 import Banner from '../Banner';
+import AboutUs from '../AboutUs';
 
 function App() {
   return (
     <div className="app">
         <p>A Bit Of Help</p>
         <Banner />
+        <AboutUs />
         <Footer />
     </div>
   );
