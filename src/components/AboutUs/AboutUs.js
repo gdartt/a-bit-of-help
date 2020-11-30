e8cc96d734bc8a290013c01562295875eb6b4978
Alex Basystyi <alexbasisty@gmail.com>
2020-11-30 13:02:02 +0100

    rendered text, added classes

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import './aboutUs.scss';
+import { aboutUsData } from '../../db/db';
 
 const AboutUs = () => {
     return (
-        <h2>Hello</h2>
+        <div>
+            <section className="text">
+                <h2 className="about__font-title--top">A bit of help</h2>
+                <h2 className="about__font-title--bottom">Fundacja</h2>
+                <h3 className="about__font-subtitle--top">Innowacyjne rozwiązania dla organizacji non-profit</h3>
+                <ul>
+                    {
+                        aboutUsData.innovativeSolutions.map((el, index) => (
+                            <li key={index} className="about__font-paragraph--top">{el}</li>
+                        ))
+                    }
+                </ul>
+            </section>
+            <section className="background"></section>
+        </div>
     )
 };
 
