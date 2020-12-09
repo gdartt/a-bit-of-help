31d684e43cf4755f32cd1b36d83c3b967c1f72b1
Kamil <36671925+SawickiKamil@users.noreply.github.com>
2020-12-09 13:03:03 +0100

    Update hero.js

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "../styles/pages/hero.scss";
 
+//according to google devs, you can use as many h1 tags as you want per page but it's not recommended by SEO experts.
 const Hero = () => {
     return (
         <div className="container">
@@ -14,6 +15,7 @@ const Hero = () => {
                         Wsparcie dla organizacji pozarządowych
                     </h1>
                 </div>
+        // you can also move it to Layout section <div className={imageClassName} />
                 <div className="background-image"></div>
             </header>
         </div>
