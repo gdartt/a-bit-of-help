d4ff1a613db2aec38b08c10a1cdce48d42fdc25e
Rafał Michniuk <38009080+RafalMichniuk@users.noreply.github.com>
2020-12-16 21:56:22 +0100

    add component list with styles

---
+++ src/db/projects.js
@@ -0,0 +1,10 @@
+export const projectsData = {
+    offerList: [
+        "Stworzymy stronę internetową.",
+        "Stworzymy małe aplikacje mobilne lub urządzenie elektroniczne dopasowane do twoich potrzeb.",
+        "Poprowadzimy kampanię reklamową w social mediach.",
+        "Napiszemy teksty na stronę, social media, blogi.",
+        "Zaprojektujemy wyjątkowy wygląd strony.",
+        "Przykładowe technologie, których możemy używać podczas realizacji projektów: HTML, CSS, PHP, JavaScript, Python, C, C++, Linux, Linux embedded, AVR, ARM, Java, Android, iOS.",
+    ],
+};
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,33 @@ import React from "react";
 
 import Layout from "../components/layout";
 
-const Projects = () => <Layout>Future components HERE</Layout>;
+import "../styles/pages/projects.scss";
+import { projectsData } from "../db/projects";
+
+const Projects = () => (
+    <Layout>
+        <section className="container projects">
+            <div className="projects__wrapper--list">
+                <h2 className="projects__title">
+                    A bit of help<span>projekty</span>
+                </h2>
+                <h3 className="projects__subtitle">
+                    ANALIZUJEMY WASZE POTRZEBY, PROJEKTUJEMY, PUBLIKUJEMY,
+                    DZIAŁAMY!
+                </h3>
+                <ul className="projects__list">
+                    {projectsData.offerList.map((item) => (
+                        <li
+                            key={item.slice(0, 33)}
+                            className="projects__paragraph"
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    </Layout>
+);
 
 export default Projects;
